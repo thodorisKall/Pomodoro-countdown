2fa54002b6ac05982ae890258adecff44aa872ce
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-12-21 02:05:44 +0200

    docs: create Footer component

---
+++ src/app/(components)/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h2 className='capitalize'>developed by thodoris kallioras</h2>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
