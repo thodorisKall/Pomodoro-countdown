703cc48c38350978f079cc82f8706a95e406fef9
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-12-22 02:13:44 +0200

    style: place Footer athe bottom right corner of the screen

--- src/app/(components)/Footer.jsx
+++ src/app/(components)/Footer.jsx
@@ -1,6 +1,8 @@
+import Link from "next/link"
+
 const Footer = () => {
   return (
-    <footer>
+    <footer className='fixed bottom-0 right-0 m-4 text-gray-500'>
       <div>
         <h2 className='capitalize'>developed by thodoris kallioras</h2>
       </div>
