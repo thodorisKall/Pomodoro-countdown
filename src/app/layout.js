37f844873cbbdfb2c21f0c801594997c933ac49c
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-12-22 02:20:42 +0200

    docs: remove default font family and add PomodoroNav in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Nav from "./(components)/Nav"
+import PomodoroNav from "./(components)/PomodoroNav"
 import Footer from "./(components)/Footer"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -13,8 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body>
         <Nav />
+        <PomodoroNav />
         {children}
         <Footer />
       </body>
