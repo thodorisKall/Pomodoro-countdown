1239e6604b87d47089de6418cee48e11b479ecbe
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-12-23 02:41:24 +0200

    style:add bg color paddings and improve flex

--- src/app/(components)/Nav.jsx
+++ src/app/(components)/Nav.jsx
@@ -1,14 +1,22 @@
 import Link from "next/link"
+import Image from "next/image"
 import { RiSettings3Fill } from "react-icons/ri"
 
 const Nav = () => {
   return (
-    <nav className='py-4 flex justify-end '>
-      <div className='flex justify-around items-center px-2 w-1/3 '>
-        <Link href='/settings'>
+    <nav className='topNav h-48  flex justify-around'>
+      <Image
+        src='/images/pomodoro-logo.png'
+        width={500}
+        height={50}
+        quality={100}
+        alt='pomodoro-logo'
+      />
+      <div className='flex justify-around items-center w-1/3 '>
+        <Link href='/settings' className='text-orange text-3xl'>
           <RiSettings3Fill />
         </Link>
-        <Link href='about'>
+        <Link href='about' className='bg-orange text-lg py-2 px-4 rounded-3xl '>
           <h2>What is Pomodoro</h2>
         </Link>
       </div>
