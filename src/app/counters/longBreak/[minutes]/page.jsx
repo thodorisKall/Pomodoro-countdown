2bde74c4f61405762af3b0edfa42adffb2649121
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-12-26 19:27:08 +0200

    docs: add image paths to call countdown component with different image

--- src/app/counters/longBreak/[minutes]/page.jsx
+++ src/app/counters/longBreak/[minutes]/page.jsx
@@ -1,8 +1,11 @@
 import Countdown from "../../../(components)/Countdown"
+
+let breakImage = "/images/relax.png"
+
 const LongBreak = ({ params }) => {
   return (
     <div>
-      <Countdown minutes={params.minutes} />
+      <Countdown minutes={params.minutes} imageBanner={breakImage} />
     </div>
   )
 }
--- src/app/counters/shortBreak/[minutes]/page.jsx
+++ src/app/counters/shortBreak/[minutes]/page.jsx
@@ -1,8 +1,9 @@
 import Countdown from "../../../(components)/Countdown"
+let breakImage = "/images/break.png"
 const ShortBreak = ({ params }) => {
   return (
     <div>
-      <Countdown minutes={params.minutes} />
+      <Countdown minutes={params.minutes} imageBanner={breakImage} />
     </div>
   )
 }
